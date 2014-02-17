6efc2595fbc8dc5e7de424b62f43ba23045713d0
Pierre-Yves Chibon <pingou@pingoured.fr>
2014-02-17 11:25:47 +0100

    pep8 fixes

--- fedora_active_user.py
+++ fedora_active_user.py
@@ -49,7 +49,7 @@ _table_keys = {
     'group_config': ['group_id', 'tag_id'],
     'group_req_listing': ['group_id', 'tag_id', 'req_id'],
     'group_package_listing': ['group_id', 'tag_id', 'package'],
-    }
+}
 
 
 _mailing_lists = [
@@ -63,7 +63,7 @@ _mailing_lists = [
     'gmane.linux.redhat.fedora.infrastructure',
     'gmane.linux.redhat.fedora.kde',
     'gmane.linux.redhat.fedora.perl'
-    ]
+]
 
 
 def _get_bodhi_history(username):
@@ -83,11 +83,13 @@ def _get_bodhi_history(username):
                                               "%Y-%m-%d %H:%M:%S")
 
         return getter
-    
+
     print('Last package update on bodhi:')
     if json_obj['updates']:
-        latest = sorted(json_obj['updates'], key=dategetter("date_submitted"))[-1]
-        print('   {0} on package {1}'.format(latest["date_submitted"], latest["title"]))
+        latest = sorted(json_obj['updates'], key=dategetter("date_submitted")
+                        )[-1]
+        print('   {0} on package {1}'.format(
+            latest["date_submitted"], latest["title"]))
     else:
         print('   No activity found on bodhi')
 
@@ -108,14 +110,15 @@ def _get_bugzilla_history(email, all_comments=False):
     log.debug('Querying bugzilla for email: {0}'.format(email))
 
     print("Bugzilla activity")
-    bugbz = bzclient.query(
-         {'emailtype1': 'substring',
-         'emailcc1': True,
-         'emailassigned_to1': True,
-         'query_format': 'advanced',
-         'order': 'Last Change',
-         'bug_status': ['ASSIGNED', 'NEW', 'NEEDINFO'],
-         'email1': email})
+    bugbz = bzclient.query({
+        'emailtype1': 'substring',
+        'emailcc1': True,
+        'emailassigned_to1': True,
+        'query_format': 'advanced',
+        'order': 'Last Change',
+        'bug_status': ['ASSIGNED', 'NEW', 'NEEDINFO'],
+        'email1': email
+    })
     print('   {0} bugs assigned, cc or on which {1} commented'.format(
         len(bugbz), email))
     # Retrieve the information about this user
@@ -134,8 +137,8 @@ def _get_bugzilla_history(email, all_comments=False):
             converted = datetime.datetime.strptime(last_com['time'].value,
                                                    "%Y%m%dT%H:%M:%S")
             print('   #{0} {1} {2}'.format(bug.bug_id,
-                                          converted.strftime('%Y-%m-%d'),
-                                          last_com['author']))
+                                           converted.strftime('%Y-%m-%d'),
+                                           last_com['author']))
 
             if not all_comments:
                 break
@@ -240,9 +243,11 @@ def _get_fedmsg_history(username):
     stream.close()
     jsonobj = json.loads(page)
     for entry in jsonobj['raw_messages']:
-        print '  - %s on %s' % (entry['meta']['subtitle'],
-        datetime.datetime.fromtimestamp(int(entry['timestamp'])
-        ).strftime('%Y-%m-%d %H:%M:%S')),
+        print '  - %s on %s' % (
+            entry['meta']['subtitle'],
+            datetime.datetime.fromtimestamp(
+                int(entry['timestamp'])
+            ).strftime('%Y-%m-%d %H:%M:%S')),
         if 'meetbot' in entry['topic']:
             if username in entry['msg']['chairs']:
                 print "(%s was a chair)" % username,
@@ -301,8 +306,8 @@ def _print_histline(entry, **kwargs):
         if event_id != other[0]:
             bad_edit = "non-matching"
         if bad_edit:
-            print('Warning: unusual edit at event {0} in table {1} ({2})'\
-                ''.format(event_id, table, bad_edit))
+            print('Warning: unusual edit at event {0} in table {1} ({2})'
+                  ''.format(event_id, table, bad_edit))
             #we'll simply treat them as separate events
             _print_histline(entry, **kwargs)
             for data in edit:
@@ -318,7 +323,8 @@ def _print_histline(entry, **kwargs):
             who = "by %(revoker_name)s"
     if table == 'tag_listing':
         if edit:
-            fmt = "%(name)s-%(version)s-%(release)s re-tagged into %(tag.name)s"
+            fmt = "%(name)s-%(version)s-%(release)s re-tagged into "\
+                  "%(tag.name)s"
         elif create:
             fmt = "%(name)s-%(version)s-%(release)s tagged into %(tag.name)s"
         else:
@@ -339,11 +345,14 @@ def _print_histline(entry, **kwargs):
             fmt = "user %(user.name)s removed from group %(group.name)s"
     elif table == 'tag_packages':
         if edit:
-            fmt = "package list entry for %(package.name)s in %(tag.name)s updated"
+            fmt = "package list entry for %(package.name)s in %(tag.name)s "\
+                  "updated"
         elif create:
-            fmt = "package list entry created: %(package.name)s in %(tag.name)s"
+            fmt = "package list entry created: %(package.name)s in "\
+                  "%(tag.name)s"
         else:
-            fmt = "package list entry revoked: %(package.name)s in %(tag.name)s"
+            fmt = "package list entry revoked: %(package.name)s in "\
+                  "%(tag.name)s"
     elif table == 'tag_inheritance':
         if edit:
             fmt = "inheritance line %(tag.name)s->%(parent.name)s updated"
@@ -360,46 +369,58 @@ def _print_histline(entry, **kwargs):
             fmt = "tag deleted: %(tag.name)s"
     elif table == 'build_target_config':
         if edit:
-            fmt = "build target configuration for %(build_target.name)s updated"
+            fmt = "build target configuration for %(build_target.name)s "\
+                  "updated"
         elif create:
             fmt = "new build target: %(build_target.name)s"
         else:
             fmt = "build target deleted: %(build_target.name)s"
     elif table == 'external_repo_config':
         if edit:
-            fmt = "external repo configuration for %(external_repo.name)s altered"
+            fmt = "external repo configuration for %(external_repo.name)s "\
+                  "altered"
         elif create:
             fmt = "new external repo: %(external_repo.name)s"
         else:
             fmt = "external repo deleted: %(external_repo.name)s"
     elif table == 'tag_external_repos':
         if edit:
-            fmt = "external repo entry for %(external_repo.name)s in tag %(tag.name)s updated"
+            fmt = "external repo entry for %(external_repo.name)s in tag "\
+                  "%(tag.name)s updated"
         elif create:
-            fmt = "external repo entry for %(external_repo.name)s added to tag %(tag.name)s"
+            fmt = "external repo entry for %(external_repo.name)s added to "\
+                  "tag %(tag.name)s"
         else:
-            fmt = "external repo entry for %(external_repo.name)s removed from tag %(tag.name)s"
+            fmt = "external repo entry for %(external_repo.name)s removed "\
+                  "from tag %(tag.name)s"
     elif table == 'group_config':
         if edit:
-            fmt = "group %(group.name)s configuration for tag %(tag.name)s updated"
+            fmt = "group %(group.name)s configuration for tag %(tag.name)s "\
+                  "updated"
         elif create:
             fmt = "group %(group.name)s added to tag %(tag.name)s"
         else:
             fmt = "group %(group.name)s removed from tag %(tag.name)s"
     elif table == 'group_req_listing':
         if edit:
-            fmt = "group dependency %(group.name)s->%(req.name)s updated in tag %(tag.name)s"
+            fmt = "group dependency %(group.name)s->%(req.name)s updated in "\
+                  "tag %(tag.name)s"
         elif create:
-            fmt = "group dependency %(group.name)s->%(req.name)s added in tag %(tag.name)s"
+            fmt = "group dependency %(group.name)s->%(req.name)s added in "\
+                  "tag %(tag.name)s"
         else:
-            fmt = "group dependency %(group.name)s->%(req.name)s dropped from tag %(tag.name)s"
+            fmt = "group dependency %(group.name)s->%(req.name)s dropped from"\
+                  " tag %(tag.name)s"
     elif table == 'group_package_listing':
         if edit:
-            fmt = "package entry %(package)s in group %(group.name)s, tag %(tag.name)s updated"
+            fmt = "package entry %(package)s in group %(group.name)s, tag "\
+                  "%(tag.name)s updated"
         elif create:
-            fmt = "package %(package)s added to group %(group.name)s in tag %(tag.name)s"
+            fmt = "package %(package)s added to group %(group.name)s in tag "\
+                  "%(tag.name)s"
         else:
-            fmt = "package %(package)s removed from group %(group.name)s in tag %(tag.name)s"
+            fmt = "package %(package)s removed from group %(group.name)s in "\
+                  "tag %(tag.name)s"
     else:
         if edit:
             fmt = "%s entry updated" % table
@@ -463,28 +484,28 @@ def setup_parser():
         prog="fedora_active_user")
     # General connection options
     parser.add_argument('--user', dest="username",
-                help="FAS username")
+                        help="FAS username")
     parser.add_argument('--email', dest="email",
-                help="FAS or Bugzilla email looked for")
+                        help="FAS or Bugzilla email looked for")
     parser.add_argument('--nofas', action='store_true',
-                help="Do not check FAS")
+                        help="Do not check FAS")
     parser.add_argument('--nokoji', action='store_true',
-                help="Do not check koji")
+                        help="Do not check koji")
     parser.add_argument('--nolists', action='store_true',
-                help="Do not check mailing lists")
+                        help="Do not check mailing lists")
     parser.add_argument('--nobodhi', action='store_true',
-                help="Do not check bodhi")
+                        help="Do not check bodhi")
     parser.add_argument('--nobz', action='store_true',
-                help="Do not check bugzilla")
+                        help="Do not check bugzilla")
     parser.add_argument('--nofedmsg', action='store_true',
-                help="Do not check fedmsg")
+                        help="Do not check fedmsg")
     parser.add_argument('--all-comments', action='store_true',
-                help="Prints the date of all the comments made by this\
-                person on bugzilla")
+                        help="Prints the date of all the comments made by this"
+                             " person on bugzilla")
     parser.add_argument('--verbose', action='store_true',
-                help="Gives more info about what's going on")
+                        help="Gives more info about what's going on")
     parser.add_argument('--debug', action='store_true',
-                help="Outputs bunches of debugging info")
+                        help="Outputs bunches of debugging info")
     return parser
 
 
